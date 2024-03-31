4639329134136c1da7238a5e6f1cb1b25f5080c9
Ali isa Tas <alisatas9292@gmail.com>
2024-03-31 13:35:55 +0200

    add xpath

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,24 +1,25 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class HomePage extends BasePage{
 
-    public SelenideElement buttonsButton = $ ( "//div[@class='element-list collapse show']//li[@class='btn btn-light '][(count(preceding-sibling::*)+1) = 5]" );
-    public SelenideElement clickMeButton = $ ("//div[@class='col-12 mt-4 col-md-6']//div[(count(preceding-sibling::*)+1) = 4]//button");
-    public SelenideElement getTextDynamicMessage = $("//p[@id='dynamicClickMessage']");
-    public SelenideElement addButton = $("//button[@id='addNewRecordButton']");
-    public SelenideElement firstNameText = $("//input[@id='firstName']");
-    public SelenideElement surNameText = $("//input[@id='lastName']");
-    public SelenideElement emailText = $("//input[@id='userEmail']");
-    public SelenideElement ageText = $("//input[@id='age']");
-    public SelenideElement salaryText = $("//input[@id='salary']");
-    public SelenideElement departmentText = $("//input[@id='department']");
-    public SelenideElement saveButton = $("//button[@id='submit']");
-    public SelenideElement editButton = $("//span[@id='edit-record-3']/svg");
-    public SelenideElement editedName=$("#app > div > div > div > div.col-12.mt-4.col-md-6 > div.web-tables-wrapper > div.ReactTable.-striped.-highlight > div.rt-table > div.rt-tbody > div:nth-child(3) > div > div:nth-child(1)");
+    public SelenideElement buttonsButton = $ ( By.xpath ( "//div[@class='element-list collapse show']//li[@class='btn btn-light '][(count(preceding-sibling::*)+1) = 5]" ));
+    public SelenideElement clickMeButton = $ (By.xpath ("//div[@class='col-12 mt-4 col-md-6']//div[(count(preceding-sibling::*)+1) = 4]//button"));
+    public SelenideElement getTextDynamicMessage = $(By.xpath ("//p[@id='dynamicClickMessage']"));
+    public SelenideElement addButton = $(By.xpath ("//button[@id='addNewRecordButton']"));
+    public SelenideElement firstNameText = $(By.xpath ("//input[@id='firstName']"));
+    public SelenideElement surNameText = $(By.xpath ("//input[@id='lastName']"));
+    public SelenideElement emailText = $(By.xpath ("//input[@id='userEmail']"));
+    public SelenideElement ageText = $(By.xpath ("//input[@id='age']"));
+    public SelenideElement salaryText = $(By.xpath ("//input[@id='salary']"));
+    public SelenideElement departmentText = $(By.xpath ("//input[@id='department']"));
+    public SelenideElement saveButton = $(By.xpath ("//button[@id='submit']"));
+    public SelenideElement editButton = $(By.xpath ("//span[@id='edit-record-3']/svg"));
+    public SelenideElement editedName=$(By.xpath ("#app > div > div > div > div.col-12.mt-4.col-md-6 > div.web-tables-wrapper > div.ReactTable.-striped.-highlight > div.rt-table > div.rt-tbody > div:nth-child(3) > div > div:nth-child(1)"));
 
 
 
